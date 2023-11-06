7b83927c312454d8b1cd9ccdc5ea02af82143bc9
Karolalbr <karol.albrechtowicz@microsoft.wsei.edu.pl>
2023-11-06 20:38:46 +0100

    Dodaj pliki projektów.

---
+++ Lab3/Controllers/ContactController.cs
@@ -0,0 +1,96 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Lab3.Controllers
+{
+    public class ContactController : Controller
+    {
+        static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        static int id = 1;
+
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = id++;
+                _contacts.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_contacts.ContainsKey(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (_contacts.ContainsKey(id))
+            {
+                _contacts.Remove(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Details(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab3/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Contact
+    {
+
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Muisz podac imie!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Imie jest zbyt długie, maksymalnie 50 znakow!")]
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Musisz podac poprawny email(brak znaku @)!")]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Numer telefonu powinen zawierać cyfry")]
+        public string Phone { get; set; }
+
+        public DateTime? Birth { get; set; }
+
+    }
+}
